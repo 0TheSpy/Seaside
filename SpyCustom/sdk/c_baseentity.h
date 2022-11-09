97fa6d590ea776500720e1f31a8749cb232ff6f0
0TheSpy <5511c282@opayq.com>
2022-11-09 04:36:24 +0300

    Update c_baseentity.h

--- SpyCustom/sdk/c_baseentity.h
+++ SpyCustom/sdk/c_baseentity.h
@@ -112,6 +112,7 @@ public:
 		NETVAR(GetViewOffset, "CBasePlayer", "m_vecViewOffset[0]", Vector);
 		NETVAR(GetTeam, "CBaseEntity", "m_iTeamNum", int);
 
+		NETVAR2(GetFlags, "DT_CSPlayer", "m_fFlags", int);
 		NETVAR2(GetNightvision, "DT_CSPlayer", "m_bNightVisionOn", bool);
 		NETVAR2(GetNightvisionAlpha, "DT_CSPlayer", "m_flNightVisionAlpha", float);
 	}; 
